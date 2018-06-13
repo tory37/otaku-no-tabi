7a91f3f3b3e8391f3aee633b01154b227e8a3568
Tory Hebert <tory37@gmail.com>
2018-06-13 09:38:28 -0500

    style changes

--- client/src/components/subscription-links/subscription-links.js
+++ client/src/components/subscription-links/subscription-links.js
@@ -11,31 +11,35 @@ class SubscriptionLink extends Component {
                 <img alt="" src="https://assets.podiant.co/img/apple.svg" />
                 <span>Apple Podcasts</span>
             </a>
-            <a className="entry castro flex row justify-center align-center" href="https://itunes.apple.com/us/podcast/otaku-no-tabi/id1390414636?mt=2">
+            <a className="entry castro flex row justify-center align-center" href="https://castro.fm/itunes/1390414636">
                 <img alt="" src="https://assets.podiant.co/img/castro.svg" />
                 <span>Castro</span>
             </a>
-            <a className="entry google flex row justify-center align-center" href="https://itunes.apple.com/us/podcast/otaku-no-tabi/id1390414636?mt=2">
+            <a className="entry google flex row justify-center align-center" href="https://play.google.com/music/m/I4z4ohsxshkp4n5drgdk5sb4d3i?t=Otaku_no_Tabi">
                 <img alt="" src="https://assets.podiant.co/img/google.svg" />
                 <span>Google Play</span>
             </a>
-            <a className="entry pocket flex row justify-center align-center" href="https://itunes.apple.com/us/podcast/otaku-no-tabi/id1390414636?mt=2">
+            <a className="entry pocket flex row justify-center align-center" href="https://pca.st/GdX2">
                 <img alt="" src="https://assets.podiant.co/img/pcast.svg" />
                 <span>Pocket Casts</span>
             </a>
-            <a className="entry radio flex row justify-center align-center" href="https://itunes.apple.com/us/podcast/otaku-no-tabi/id1390414636?mt=2">
+            <a className="entry overcast flex row justify-center align-center" href="http://feeds.podiant.co/otakunotabi/rss.xml">
+                <img alt="" src="https://assets.podiant.co/img/overcast.svg" />
+                <span>Overcast</span>
+            </a>
+            <a className="entry radio flex row justify-center align-center" href="https://play.radiopublic.com/otaku-no-tabi-WDRN2A">
                 <img alt="" src="https://assets.podiant.co/img/radiopublic.svg" />
                 <span>RadioPublic</span>
             </a>
-            <a className="entry stitcher flex row justify-center align-center" href="https://itunes.apple.com/us/podcast/otaku-no-tabi/id1390414636?mt=2">
+            <a className="entry stitcher flex row justify-center align-center" href="https://www.stitcher.com/podcast/tory-hebert/otaku-no-tabi?refid=stpr">
                 <img alt="" src="https://assets.podiant.co/img/stitcher.svg" />
                 <span>Stitcher</span>
             </a>
-            <a className="entry tunein flex row justify-center align-center" href="https://itunes.apple.com/us/podcast/otaku-no-tabi/id1390414636?mt=2">
+            <a className="entry tunein flex row justify-center align-center" href="https://tunein.com/podcasts/Anime/Otaku-no-Tabi-p1127307/">
                 <img alt="" src="https://assets.podiant.co/img/tunein.svg" />
                 <span>TuneIn</span>
             </a>
-            <a className="entry rss flex row justify-center align-center" href="https://itunes.apple.com/us/podcast/otaku-no-tabi/id1390414636?mt=2">
+            <a className="entry rss flex row justify-center align-center" href="http://feeds.podiant.co/otakunotabi/rss.xml">
                 <img alt="" src="https://assets.podiant.co/img/rss.svg" />
                 <span>RSS</span>
             </a>
